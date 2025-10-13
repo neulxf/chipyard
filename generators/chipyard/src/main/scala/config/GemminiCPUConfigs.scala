748fb57c3ecd2a05071569043ea9a85f95e3400b
lxf <lxf191375@163.com>
2025-10-14 01:23:36 +0800

    init

---
+++ generators/chipyard/src/main/scala/config/GemminiCPUConfigs.scala
@@ -0,0 +1,18 @@
+package chipyard
+
+import boom.v4.common._
+import freechips.rocketchip.rocket._
+
+object CustomGemmminiCPUConfigs {
+  // Default CPU configs
+  type RocketBigCores = WithNBigCores
+  type RocketMedCores = WithNMedCores
+  type RocketSmallCores = WithNSmallCores
+
+  type BoomLargeCores = WithNLargeBooms
+  type BoomMedCores = WithNMediumBooms
+  type BoomSmallCores = WithNMediumBooms
+
+  // Specify which CPU configs you want to build here
+  type CustomCPU = RocketBigCores
+}
